83e96ce9c5876e0c4f11cab21319377d8c826a90
septianaana <45724156+septianaana@users.noreply.github.com>
2019-01-05 12:22:27 +0700

    Add files via upload

---
+++ menghitung jarak tempuh.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+    int C,M,E;
+    cout<<"masukan kecepatan:";
+    cin>>C;
+    cout<<"masukan massa :";
+    cin>>M;
+    E=C*C*C;
+    cout<<"hasil percepatan cahaya adalah"<<E;
+    return 0;
+}
